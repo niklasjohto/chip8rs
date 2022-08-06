b1b43c9beb8bc31e41ad536e2e63fb50f68450a9
gaasefod <gaasefod@tutanota.com>
2022-08-06 18:04:27 +0200

    Stack methods, Font Sprites & CPU Tick

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,34 @@ const NUM_KEYS: usize = 16;
 
 const START_ADDR: u16 = 0x200;
 
+const FONTSET_SIZE: usize = 80;
+
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+];
+
 pub struct Emulator {
-    pc: u16,
+    program_counter: u16,
     ram: [u8; RAM_SIZE],
     display: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
     v_register: [u8; NUM_VREGS],
     i_register: u16,
-    sp: u16,
+    stack_pointer: u16,
     stack: [u16; STACK_SIZE],
     keys: [bool; NUM_KEYS],
     delay_timer: u8,
@@ -23,17 +44,57 @@ pub struct Emulator {
 
 impl Emulator {
     pub fn new() -> Self {
-        Self { 
-            pc: START_ADDR, 
+        let mut new_emulator = Self { 
+            program_counter: START_ADDR, 
             ram: [0; RAM_SIZE], 
             display: [false; SCREEN_WIDTH * SCREEN_HEIGHT], 
             v_register: [0; NUM_VREGS], 
             i_register: 0, 
-            sp: 0, 
+            stack_pointer: 0, 
             stack: [0; STACK_SIZE], 
             keys: [false; NUM_KEYS], 
             delay_timer: 0, 
             sound_timer: 0 
-        }
+        };
+
+        new_emulator.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
+        new_emulator
+    }
+
+    fn push(&mut self, value: u16) {
+        self.stack[self.stack_pointer as usize] = value;
+        self.stack_pointer += 1;
+    }
+
+    fn pop(&mut self) -> u16 {
+        self.stack_pointer -= 1;
+        self.stack[self.stack_pointer as usize]
+    }
+
+    pub fn reset(&mut self) {
+        self.program_counter = START_ADDR;
+        self.ram = [0; RAM_SIZE];
+        self.display = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v_register = [0; NUM_VREGS];
+        self.i_register = 0;
+        self.stack_pointer = 0;
+        self.stack = [0; STACK_SIZE];
+        self.keys = [false; NUM_KEYS];
+        self.delay_timer = 0;
+        self.sound_timer = 0;
+        self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+    }
+
+    pub fn tick(&mut self) {
+        let opcode = self.fetch();
+    }
+
+    fn fetch(&mut self) -> u16 {
+        let higher_byte = self.ram[self.program_counter as usize] as u16;
+        let lower_byte = self.ram[(self.program_counter + 1) as usize] as u16;
+        let opcode = (higher_byte << 8) | lower_byte;
+        self.program_counter += 2;
+        opcode
     }
 }
